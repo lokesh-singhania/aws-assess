815738d9980889a61b5772fb80d2b004a2a0c076
Lokesh Singhania <lkssgn@gmail.com>
2024-01-12 14:40:41 +0000

    test

--- backend/test.ts
+++ backend/test.ts
@@ -15,8 +15,30 @@ async function basicLatencyTest() {
     console.log(`Latency: ${performance.now() - start} ms`);
 }
 
+async function reproTest() {
+    await app.post("/reset").expect(204);
+    const charge=10
+    const number=6
+    const initial=100
+    const expected = initial - number*charge
+    const pmss=[]
+    for(let i=1;i<number;i++){ // intentional number-1
+        pmss.push(app.post("/charge"));
+    }
+    await Promise.allSettled(pmss);
+    sleep(1000)   
+    const response=await app.post("/charge")
+    console.log(`Expected: ${expected}`)   
+    console.log(`Actual: ${response.body.remainingBalance}`);
+}
+
+function sleep(ms:number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function runTests() {
     await basicLatencyTest();
+    await reproTest();
 }
 
 runTests().catch(console.error);
